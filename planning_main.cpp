0023fc68a19ac0175b1a39a942ab419920ab9ecf
WeiXiangTan <wei_xiangtan@dahuatech.com>
2024-07-01 15:48:27 +0800

    version 0.0.1

---
+++ planning_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "process.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "planing start" << endl;
+    Process pro;
+    pro.planProcess();
+    cout << "planning end" << endl;
+
+    return 0;
+}
